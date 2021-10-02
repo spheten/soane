7ece52c438658ba9a4d0c93f2f4b358c59392031
spheten <86143135+spheten@users.noreply.github.com>
2021-10-03 08:30:13 +1000

    Added soane.tools package.

--- soane/__init__.py
+++ soane/__init__.py
@@ -5,3 +5,5 @@ Package definition for 'soane'.
 VERSION_DATE   = '2021-10-03'
 VERSION_NUMBER = '0.0.0'
 VERSION_STRING = f'Soane version {VERSION_NUMBER} ({VERSION_DATE}).'
+
+from soane import tools
